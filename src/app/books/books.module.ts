19900a142a739869d98c87bec6bcb596d4a14f76
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-07-02 19:37:06 -0300

    import module

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -5,13 +5,14 @@ import { BooksRoutingModule } from './books-routing.module';
 import { BooksComponent } from './containers/books/books.component';
 import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { BooksFormComponent } from './containers/books-form/books-form.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 @NgModule({
   declarations: [
     BooksFormComponent
   ],
-  imports: [CommonModule, BooksRoutingModule, AppMaterialModule],
+  imports: [CommonModule, BooksRoutingModule, AppMaterialModule, MatFormFieldModule],
 })
 export class BooksModule {}
 export { BooksComponent };
